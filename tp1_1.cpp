a4c3fde958088a2c37320ed3e51b6706f0372d5f
juancastelli1 <48569961+juancastelli1@users.noreply.github.com>
2019-03-21 17:20:55 -0300

    Update tp1_1.cpp

--- tp1_1.cpp
+++ tp1_1.cpp
@@ -9,7 +9,7 @@ int main (void){
 	printf("Direccion de memoria almacenada: %p\n", p);//El punto 2 y 3 son iguales porque el puntero almacena la direccion de memoria de la variable, lo cual es diferente a la direccion de memoria donde se almacena el puntero
 	printf("Direccion de memoria de la variable: %p\n", &var);
 	printf("Direccion de memoria del puntero: %p\n", &p);
-	printf("Tamaño de memoria: %d\n", 	sizeof(var));
+	printf("Tamaño de memoria: %d\n",sizeof(var));
 	return 0;
 
 }
